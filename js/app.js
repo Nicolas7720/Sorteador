bda6be37956f698a50b15a6c3ca89a76450280d4
Nikao777 <139736216+Nicolas7720@users.noreply.github.com>
2025-03-12 08:05:40 -0300

    implementei condições de validaçao para iniciar o sorteio

--- js/app.js
+++ js/app.js
@@ -4,25 +4,61 @@ const sortear = () => {
   const inputNumFinal = parseInt(document.querySelector("#ate").value);
   const textoSorteados = document.querySelector("#resultado");
 
-  const sorteados = [];
+  if (
+    isNaN(inputQuantidade) ||
+    isNaN(inputNumInicial) ||
+    isNaN(inputNumFinal)
+  ) {
+    alert("Alguma informação está Faltando ou é Invalida. Verifique!");
+    return;
+  } else if (inputNumInicial > inputNumFinal) {
+    alert(
+      "O campo: 'Do número' deve ser maior que o campo: 'Até o número'. Verifique!"
+    );
+    return;
+  } else {
+    if (inputQuantidade > inputNumFinal - inputNumInicial + 1) {
+      alert(
+        "Campo 'Quantidade' deve ser menor ou igual ao intervalo informado no campo 'Do número' até o campo 'Até o número'. Verifique!"
+      );
+      return;
+    } else {
+      const sorteados = [];
 
-  while (sorteados.length < inputQuantidade) {
-    const num = obterNumAleatório(inputNumInicial, inputNumFinal);
-    if (!sorteados.includes(num)) {
-      sorteados.push(num);
+      while (sorteados.length < inputQuantidade) {
+        const num = obterNumAleatório(inputNumInicial, inputNumFinal);
+        if (!sorteados.includes(num)) {
+          sorteados.push(`${num} `);
+        }
+      }
+      textoSorteados.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
     }
   }
-
-  textoSorteados.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+  alterarStatusBotao();
 };
 const obterNumAleatório = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const alterarStatusBotao = () => {
+  const btnIniciar = document.querySelector(".container__botao-desabilitado");
+  if (btnIniciar.classList.contains("container__botao-desabilitado")) {
+    btnIniciar.classList.remove("container__botao-desabilitado");
+    btnIniciar.classList.add("container__botao");
+  }
 };
 
 const reiniciar = () => {
   const inputQuantidade = (document.querySelector("#quantidade").value = "");
   const inputNumInicial = (document.querySelector("#de").value = "");
   const inputNumFinal = (document.querySelector("#ate").value = "");
+  const btnIniciarModificado =
+    document.querySelectorAll(".container__botao")[1];
+  if (btnIniciarModificado.classList.contains("container__botao")) {
+    btnIniciarModificado.classList.remove("container__botao");
+    btnIniciarModificado.classList.add("container__botao-desabilitado");
+  }
+
   const textoSorteados = document.querySelector("#resultado");
   textoSorteados.innerHTML = `<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>`;
 };
